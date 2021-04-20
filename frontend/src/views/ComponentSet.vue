823aaffd663bfcc3af2bd72f48bf0c60a2ab690b
장주빈 <sunshine4429@gmail.com>
2021-04-20 13:53:01 +0900

    feat: add new vue project

---
+++ frontend/src/views/ComponentSet.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component-set">
+    <Button 
+      test="키아아"
+    />
+  </div>
+</template>
+
+<script>
+import Button from '@/components/Button.vue';
+
+export default {
+  name: 'ComponentSet',
+  components: {
+    Button,
+  },
+}
+</script>
