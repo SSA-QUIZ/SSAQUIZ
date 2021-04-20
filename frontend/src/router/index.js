2375c2705e6d6a2fb6e919bfb1fddd44c2fa3a84
chaewon <chechekk0@naver.com>
2021-04-20 16:07:04 +0900

    feat: add vuetify

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ComponentSet from '../views/ComponentSet.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'ComponentSet',
     component: ComponentSet
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
 ]
 
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg">
+    <div id="login">고고</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style>
+
+</style>
