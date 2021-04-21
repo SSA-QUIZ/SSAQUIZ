5f5cbab5da016abe182fca5f46bc242746ae5228
최나현 <cnnnnh@gmail.com>
2021-04-21 18:02:33 +0900

    feat: add SolvingQuiz, Header, ProgressBar, QuizButton, MultipleChoice

---
+++ frontend/src/components/common/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <v-row id="header-row">
+      <v-col cols="4">
+        <img
+          width="100%"
+          src="@/assets/images/SSAQUIZ.png">
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col cols="7" align="end">
+        <div id="nickname">{{ nickname }}</div>
+        <div id="score">{{ score }}</div>
+      </v-col>
+    </v-row>
+  </header>
+</template>
+
+<script>
+export default {
+  props: [
+    'nickname',
+    'score'
+  ]
+}
+</script>
+
+<style>
+
+</style>
