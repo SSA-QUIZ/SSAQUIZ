547a3ba2890600926a99407c123d041991b372e9
chaewon <chechekk0@naver.com>
2021-04-21 17:35:30 +0900

    feat: signup page, login page 설계

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-list-item>
+    <v-list-item-content>
+      <v-text-field
+        label="ID를 입력해주세요."
+        solo
+        hide-details
+        class="mt-10 mb-3"
+      ></v-text-field>
+      <v-text-field
+        label="PW를 입력해주세요."
+        solo
+        hide-details
+        class="mb-3"
+      ></v-text-field>
+      <v-btn class="login-btn mb-3">로그인 하기</v-btn>
+      <v-btn class="login-btn mb-3">구글로그인 하기</v-btn>
+      <p class="atag">퀴즈를 풀러 오셨어요?</p>
+      <p class="atag" @click="moveToSignup">아직 저희 회원이 아니신가요?</p>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  methods: {
+    moveToSignup: function () {
+      this.$emit("signal", true);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/SignupForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="signup">
+    <v-tabs v-model="tab" centered background-color="transparent">
+      <v-tab v-for="item in items" :key="item">
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab" style="background-color: transparent">
+      <v-tab-item>
+        <v-card> 로컬로그인 </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card> 소셜 로그인 </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Signup",
+  data: function () {
+    return {
+      tab: null,
+      items: ["SSAQUIZ", "Google"],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
