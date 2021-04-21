e5c0881de0a0e183d2a7902d8c0f8d166ebd71ce
chaewon <chechekk0@naver.com>
2021-04-21 23:06:18 +0900

    feat: 로그인 & 회원가입 페이지 제작
    - Input Button와 InputBox 컴포넌트를 이용하여 로그인, 회원가입을
      구성했다.
    - LoginForm과 SignupForm을 생성했다.

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,8 +2,14 @@
   <v-container id="login" fluid>
     <v-card flat class="mx-auto card-bg">
       <v-img src="@/assets/images/SSAQUIZ.png"></v-img>
-      <LoginForm v-if="loadSignUp === false" @signal="moveToSignup" />
-      <SignupForm v-else @signal="moveToLogin" />
+      <v-window v-model="step">
+        <v-window-item>
+          <LoginForm @move-to-signup="moveToSignup" />
+        </v-window-item>
+        <v-window-item>
+          <SignupForm @move-to-login="moveToLogin" />
+        </v-window-item>
+      </v-window>
     </v-card>
   </v-container>
 </template>
@@ -19,15 +25,15 @@ export default {
   },
   data: function () {
     return {
-      loadSignUp: false,
+      step: 0,
     };
   },
   methods: {
-    moveToSignup: function (val) {
-      this.loadSignUp = val;
+    moveToSignup: function () {
+      this.step++;
     },
-    moveToLogin: function (val) {
-      this.loadSignUp = val;
+    moveToLogin: function () {
+      this.step--;
     },
   },
 };
