bc365ade34920e596fa3b80367fb0f998e49dc57
chaewon <chechekk0@naver.com>
2021-04-21 17:35:30 +0900

    feat: signup page, login page 설계

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,32 +2,33 @@
   <v-container id="login" fluid>
     <v-card flat class="mx-auto card-bg">
       <v-img src="@/assets/images/SSAQUIZ.png"></v-img>
-      <!-- <v-list-item>
-        <v-list-item-content>
-          <v-text-field
-            label="ID를 입력해주세요."
-            solo
-            hide-details
-            class="mt-10 mb-3"
-          ></v-text-field>
-          <v-text-field
-            label="PW를 입력해주세요."
-            solo
-            hide-details
-            class="mb-3"
-          ></v-text-field>
-          <v-btn class="login-btn mb-3">로그인 하기</v-btn>
-          <v-btn class="login-btn mb-3">구글로그인 하기</v-btn>
-          <a href="" class="atag">퀴즈를 풀러 오셨어요?</a>
-          <a href="" class="atag">아직 저희 회원이 아니신가요?</a>
-        </v-list-item-content>
-      </v-list-item> -->
+      <LoginForm v-if="loadSignUp === false" @signal="moveToSignup" />
+      <SignupForm v-else @signal="moveToLogin" />
     </v-card>
   </v-container>
 </template>
 
 <script>
+import LoginForm from "@/components/LoginForm.vue";
+import SignupForm from "@/components/SignupForm.vue";
 export default {
   name: "Login",
+  components: {
+    LoginForm,
+    SignupForm,
+  },
+  data: function () {
+    return {
+      loadSignUp: false,
+    };
+  },
+  methods: {
+    moveToSignup: function (val) {
+      this.loadSignUp = val;
+    },
+    moveToLogin: function (val) {
+      this.loadSignUp = val;
+    },
+  },
 };
 </script>
