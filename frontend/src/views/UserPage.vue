968f32748940a95ecf3855a261309a63ee77d648
최나현 <cnnnnh@gmail.com>
2021-04-23 01:04:16 +0900

    docs: update README.md

---
+++ frontend/src/views/UserPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="background-color: #CFE1F6; height: 100%;">
+    <Header />
+    <div style="display: flex; height: 85%;">
+      <div style="display: flex; background-color: gray; width:50%; height: 100%; align-items: center;">
+        <span style="font-size: 10rem; color: #4F37DE;"><i class="fas fa-user-circle"></i></span>
+      </div>
+      <div style="background-color: blue; width:50%; height: 100%;"></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/Header.vue';
+export default {
+  name: 'UserPage',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+
+</style>
