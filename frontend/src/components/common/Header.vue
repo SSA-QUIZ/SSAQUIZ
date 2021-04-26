05a11b591ecda04a37995c5768dde5c1fbb359ba
최나현 <cnnnnh@gmail.com>
2021-04-26 14:47:13 +0900

    refactor: change vuetify to css

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -1,29 +1,58 @@
 <template>
   <header>
-    <v-row id="header-row">
-      <v-col cols="4">
-        <img
-          width="100%"
-          src="@/assets/images/SSAQUIZ.png">
-      </v-col>
-      <v-spacer></v-spacer>
-      <v-col cols="7" align="end">
-        <div id="nickname">{{ nickname }}</div>
-        <div id="score">{{ score }}</div>
-      </v-col>
-    </v-row>
+    <div>
+      <img width="40%" src="@/assets/images/SSAQUIZ.png">
+    </div>
+    <div id="user-info" v-if="mode == 'student'">
+      <div id="user-info__nickname">{{ nickname }}</div>
+      <div id="user-info__score">{{ score }}</div>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
+  name: 'Header',
   props: [
     'nickname',
     'score'
-  ]
+  ],
+  data: function () {
+    return {
+      mode: 'student'
+    }
+  }
 }
 </script>
 
 <style>
-
+header {
+	height: 15%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+  margin-top: -0.5%;
+  margin-bottom: -2%;
+	padding: 0vh 3vh 0vh 3vh;
+}
+#user-info {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+#user-info__nickname {
+  width:8vh;
+	font-family: "Jua";
+	font-size: 3.5vh;
+	font-weight: 900;
+	margin-right: 3%;
+}
+#user-info__score {
+	font-size: 3.5vh;
+	font-weight: 900;
+	color: rgb(255, 255, 255);
+	background-color: rgb(170, 170, 170);
+	border-radius: 15px;
+	padding: 1vh 3vh 1vh 3vh;
+}
 </style>
