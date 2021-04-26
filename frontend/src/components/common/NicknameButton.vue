9adf3a0fe4f4f5d706cec3b543ef68ea77ddbaf8
장주빈 <sunshine4429@gmail.com>
2021-04-26 13:19:34 +0900

    feat: add LobbyPageT and NicknameButton component
    선생님이 퀴즈를 시작했을 때 보여지는 페이지와 거기에 들어갈 닉네임 버튼
    컴포넌트를 만들었다.

---
+++ frontend/src/components/common/NicknameButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-chip
+    class="nicknameButton"
+    large
+  >
+    {{student.nickname}}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  name: "NicknameButton",
+  props: ['student', 'index'],
+  mounted: function () {
+    console.log(this.student)
+    document.getElementsByClassName("nicknameButton")[this.index].style.backgroundColor = this.student.color
+  },
+}
+</script>
+<style>
+.nicknameButton {
+  margin: 1vh;
+  vertical-align: baseline;
+}
+
+.v-chip__content {
+  font-family: Jua;
+  font-size: 30px;
+}
+
+</style>
