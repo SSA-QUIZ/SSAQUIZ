ad68ff8025c547ec047ec8d46b8d6dcfc1e69947
sungjin <tjdwls3469@gmail.com>
2021-04-27 11:11:01 +0900

    chore: modify project structure

---
+++ backend/ssaquiz-production/src/main/java/com/ssafy/ssaquiz/SsaquizApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssaquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsaquizApplication.class, args);
+    }
+
+}
