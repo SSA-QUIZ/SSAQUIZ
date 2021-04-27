e4fd711878faf11ecb6ed0f9fc89949740a86abb
chaewon <chechekk0@naver.com>
2021-04-27 22:58:11 +0900

    feat: Image file upload in UserInfoPage.vue
    - 회원 정보 수정 페이지의 레이아웃과 디자인을 조정했다.
    - input 태그를 이용하여 이미지 파일을 불러왔다.
    - 편리한 사용감을 위해 hover 효과를 추가했다.
    - 회원가입시 유저 정보를 vuex에 저장했다.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,15 +6,22 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     googleLoginToken: '',
+    userInfo: [],
   },
   mutations: {
     SET_GOOGLE_LOGIN_TOKEN: function (state, value) {
       state.googleLoginToken = value;
+    },
+    SET_USER_INFO: function (state, value) {
+      state.userInfo = value;
     }
   },
   actions: {
     setGoogleLoginToken: function ({ commit }, value) {
       commit('SET_GOOGLE_LOGIN_TOKEN', value);
+    },
+    setUserInfo: function ({ commit }, value) {
+      commit('SET_USER_INFO', value);
     }
   },
   modules: {
