5ad9d7878eda6590c5f5a5f53f4f12534759473e
최나현 <cnnnnh@gmail.com>
2021-04-27 10:22:18 +0900

    feat: add QuizResultsPage
    - 학생 정오답 페이지를 생성하였다.
    - css 명명법에 따라 UserPage의 css 선택자 이름을 변경하였다.
    - Header 안 닉네임 정렬을 변경하였다.

---
+++ frontend/src/views/QuizResultsPage/CorrectAnswer.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="quiz-results-container--blue">
+    <Header mode="on" nickname="[서울_3반_최나현]" score="354" />
+    <div id="quiz-results-content">
+      <p id="title">대단해요!</p>
+      <p id="emoticon">🥰</p>
+      <p>+ {{ quizScore }}🔥</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/Header.vue';
+export default {
+  name: 'CorrectAnswer',
+  components: {
+    Header
+  },
+  data: function () {
+    return {
+      quizScore: 324
+    }
+  }
+
+}
+</script>
+
+<style>
+#quiz-results-container--blue {
+	background-color: #7CB1FF;
+	height: 100%;
+}
+
+#quiz-results-content {
+	height: 85%;
+	display: flex;
+	flex-flow: column;
+	justify-content: center;
+	align-items: center;
+	font-size: 4rem;
+	font-family: Jua;
+}
+
+#quiz-results-content > #title {
+	font-size: 7.5rem;
+}
+
+#quiz-results-content > #emoticon {
+	font-size: 16rem;
+}
+
+@media (max-width: 700px) {
+	#quiz-results-content {
+		font-size: 2rem;
+	}
+
+	#quiz-results-content > #title {
+		font-size: 4rem;
+	}
+
+	#quiz-results-content > #emoticon {
+	font-size: 10rem;
+	}
+}
+</style>
