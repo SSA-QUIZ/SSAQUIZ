dbd2d9cbd9b739f340ddca5aca0682177e9f1bef
sungjin <tjdwls3469@gmail.com>
2021-04-28 21:38:32 +0900

    fix: quiz production exception handling

--- backend/ssaquiz-production/src/main/java/com/ssafy/ssaquiz/controller/ProduceController.java
+++ backend/ssaquiz-production/src/main/java/com/ssafy/ssaquiz/controller/ProduceController.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @CrossOrigin(origins = {"*"})
-@RequestMapping("/produce")
 @RestController
 public class ProduceController {
     @Autowired
@@ -28,8 +27,17 @@ public class ProduceController {
                               @RequestParam("answer") String answer, @RequestParam("orderedAnswer") List<String> orderedAnswer,
                               @RequestParam("answerList") List<String> answerList, @RequestParam("time") int time,
                               @RequestParam("scoreFactor") int scoreFactor, @RequestParam("type") int type) {
+        BasicResponse result = new BasicResponse();
+
+        if(objectId == null) {
+            result.status = false;
+            result.data = "문제집 조회 실패(null)";
+            return result;
+        }
+
         Workbook workbook = workbookService.findWorkbook(objectId);
         String imgPath = s3Service.upload(inputFile);
+
         return workbookService.insertSlide(workbook, category, question, imgPath, answer,
                                            orderedAnswer, answerList, time, scoreFactor, type);
     }
@@ -40,6 +48,12 @@ public class ProduceController {
     @GetMapping("/workbook/{objectId}")
     public BasicResponse findWorkbook(@PathVariable("objectId") String objectId) {
         BasicResponse result = new BasicResponse();
+
+        if(objectId == null) {
+            result.status = false;
+            result.data = "문제집 조회 실패(null)";
+            return result;
+        }
 
         result.status = true;
         result.data = "문제집 조회 성공";
