c60ee446b42bd560d32560914d9074503ed380c8
chaewon <chechekk0@naver.com>
2021-04-28 15:56:56 +0900

    fix: Modify Image name

--- frontend/src/views/AnswerPage.vue
+++ frontend/src/views/AnswerPage.vue
@@ -2,7 +2,7 @@
   <div id="answer-page-container">
     <Header mode="off" />
     <ProgressBar index="2" all="12" />
-    <Quiz title="이 동물은 무엇일까요?" image="@/assets/images/파랑고래_아이콘.png" />
+    <Quiz title="이 동물은 무엇일까요?" image="@/assets/images/Default.png" />
     <QuizButton color="#7cb1ff" icon="fa fa-car" answer="고래" id="answer-div" />
     <div style="height: 3%;"></div>
   </div>
