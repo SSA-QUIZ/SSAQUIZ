1f3b3bdd2182154a9c5b47620cd0e27d22f64ca1
chaewon <chechekk0@naver.com>
2021-04-30 16:26:40 +0900

    feat: AwaitPage.vue Design & Layout
    퀴즈 대기 페이지를 제작했다.
    - 각 요소들의 레이아웃을 설정했다.
    - 한 문제가 종료될 때 대기하는 페이지이므로, 고래 애니메이션을 추가하여
      동적인 페이지를 만들었다.
    - 모바일에서도 볼 수 있도록 반응형으로 제작했다.

---
+++ frontend/src/views/AwaitPage.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="await">
+    <!-- 고래 배경 -->
+    <div class="wheel">
+      <img class="inner-wheel" src="@/assets/images/pinkWhale.png" alt="SSAQUIZ">
+    </div>
+    <!-- 메인 -->
+    <div class="container">
+      <div class="row">
+        <img src="@/assets/images/SSAQUIZ.png" alt="SSAQUIZ">
+      </div>
+      <div class="row">
+        <h1>남은시간</h1>
+      </div>
+      <div class="row">
+        <span class="text-2">5초</span>
+      </div>
+      <div class="row">
+        <h1 class="msg">잘 하고 있어요!</h1>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AwaitPage"
+}
+</script>
+
+<style scoped>
+
+#await {
+  background-color: #cfe1f6;
+  height: 100%;
+  position: relative;
+  overflow:hidden;
+}
+
+#await .container {
+  width: 100%
+}
+
+#await .row {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+#await .row > h1,
+#await .row > img {
+  z-index: 2;
+}
+
+#await .container .row > img {
+  height: 80px;
+  margin-top: 2%;
+  margin-bottom: 7%;
+}
+
+#await .container h1 {
+  color: black;
+  font-size: 65px;
+  font-family: Jua;
+}
+
+#await .container span {
+  color: black;
+  font-size: 200px;
+  font-family: Jua;
+  margin-bottom: 7%;
+}
+
+#await .container .row .msg {
+  position: absolute;
+  bottom: 100px;
+}
+
+
+/* 고래 rotation 배경*/
+.wheel-con{
+  background-color: #f2f2f2;
+  height: 100%;
+  position: relative;
+  overflow:hidden;
+}
+.wheel{
+  position: absolute;
+  left:50%;
+  display: block;
+  width: 100px;
+  height: 100px;
+  transform-origin: center 500%;
+  animation: spin 6s linear infinite;
+}
+
+.inner-wheel{
+  top: -150px;
+  left: -50px;
+  width: 300px;
+  height: 300px;
+
+  transform-origin: center center;
+  transform: scaleY(-1);
+}
+
+@keyframes spin {
+  50% { transform: rotate(180deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
---
+++ frontend/src/views/Test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wheel-con">
+    <div class="wheel">
+      <img class="inner-wheel" src="@/assets/images/pinkWhale.png" alt="SSAQUIZ">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test"
+}
+</script>
+
+<style scoped>
+.wheel-con{
+  background-color: #f2f2f2;
+  height: 100%;
+  position: relative;
+  overflow:hidden;
+}
+.wheel{
+  position: absolute;
+  left:50%;
+  display: block;
+  width: 100px;
+  height: 100px;
+  transform-origin: center 500%;
+  animation: spin 6s linear infinite;
+}
+
+.inner-wheel{
+  top: -150px;
+  left: -50px;
+  width: 300px;
+  height: 300px;
+
+  transform-origin: center center;
+  transform: scaleY(-1);
+}
+
+
+@keyframes spin {
+  50% { transform: rotate(180deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
