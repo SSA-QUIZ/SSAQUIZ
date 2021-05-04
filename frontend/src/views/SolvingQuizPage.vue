430fb70526bea69d9dc406371044685fa0041e45
chaewon <chechekk0@naver.com>
2021-05-04 21:42:00 +0900

    chore: Organize components
    - 아직 기능을 넣지 않을 주관식 단답형 페이지를 숨겼다.

--- frontend/src/views/SolvingQuizPage.vue
+++ frontend/src/views/SolvingQuizPage.vue
@@ -2,24 +2,24 @@
   <div style="background-color: #f2f2f2; height: 100%; position: relative;">
     <Header mode="userInfo" nickname="[서울_3반_최나현]" score="950" />
     <ProgressBar index="2" all="12" />
-    <!-- <MultipleChoice :choice="choice" v-show="type == 'multiple' || type == 'vote'" /> -->
-    <ShortAnswer/>
+    <MultipleChoice :choice="choice" v-show="type == 'multiple' || type == 'vote'" />
+    <!-- <ShortAnswer/> -->
   </div>
 </template>
 
 <script>
 import Header from '@/components/common/Header.vue';
 import ProgressBar from '@/components/common/ProgressBar.vue';
-// import MultipleChoice from '@/components/QuizTemplate/MultipleChoice.vue';
-import ShortAnswer from '@/components/QuizTemplate/ShortAnswer.vue';
+import MultipleChoice from '@/components/QuizTemplate/MultipleChoice.vue';
+// import ShortAnswer from '@/components/QuizTemplate/ShortAnswer.vue';
 
 export default {
   name: 'SolvingQuizPage',
   components: {
     Header,
     ProgressBar,
-    // MultipleChoice,
-    ShortAnswer
+    MultipleChoice,
+    // ShortAnswer
   },
   data: function () {
     return {
