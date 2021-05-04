d3fd4fe56229f0e8a1d0c5949edd8c889de81dcc
최나현 <cnnnnh@gmail.com>
2021-05-04 10:42:51 +0900

    feat: add LoadingPage
    - 퀴즈 진행 전 3초 카운트하는 페이지를 제작하였다.

---
+++ frontend/src/views/LoadingPage.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="loading-page-container">
+    <div id="loading-page-timer" class="loding-page__animation">{{ second }}</div>
+    <div id="loading-page-start" class="loding-page__animation" style="display: none;">start!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingPage',
+  data: function () {
+    return {
+      second: 3,
+    }
+  },
+  created: function () {
+    this.changeSecound();
+  },
+  methods: {
+    countDown: function () {
+      if (this.second == 1) {
+        document.getElementById('loading-page-timer').style.display = "none";
+        document.getElementById('loading-page-start').style.display = "block";
+      }
+      else if (this.second == 0) {
+        document.getElementById('loading-page-start').style.display = "none";
+        // 여기에서 퀴즈 진행 페이지로 넘어가면 됩니다!
+        // this.$router.push({ name: "퀴즈 진행 페이지" })
+      }
+      this.second--;
+    },
+    changeSecound: function () {
+      this.second = setInterval(() => this.countDown(), 1000);
+    }
+  }
+}
+</script>
+
+<style scoped>
+#loading-page-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #4f37de;
+  height: 100%;
+}
+
+@keyframes jello-horizontal {
+  0% {
+    -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+  }
+  30% {
+    -webkit-transform: scale3d(1.25, 0.75, 1);
+            transform: scale3d(1.25, 0.75, 1);
+  }
+  40% {
+    -webkit-transform: scale3d(0.75, 1.25, 1);
+            transform: scale3d(0.75, 1.25, 1);
+  }
+  50% {
+    -webkit-transform: scale3d(1.15, 0.85, 1);
+            transform: scale3d(1.15, 0.85, 1);
+  }
+  65% {
+    -webkit-transform: scale3d(0.95, 1.05, 1);
+            transform: scale3d(0.95, 1.05, 1);
+  }
+  75% {
+    -webkit-transform: scale3d(1.05, 0.95, 1);
+            transform: scale3d(1.05, 0.95, 1);
+  }
+  100% {
+    -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+  }
+}
+.loding-page__animation {
+	-webkit-animation: jello-horizontal 1s steps(2, end) infinite both;  
+  font-size : 15rem;
+  font-family : Jua;
+  font-weight: bold;
+  color: white;
+}
+
+</style>
