821685524fa92e509e85769e132aafc610b8434e
chaewon <chechekk0@naver.com>
2021-05-04 15:22:10 +0900

    chore: Organize components
    - 다음단계 버튼을 컴포넌트화했다.
    - 중간점검페이지(ImterimScore.vue)의 각 요소를 컴포넌트화했다.
    - 애니메이션 효과를 effects폴더에 생성했다.

---
+++ frontend/src/components/common/NextStepButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="step-button"><img src="@/assets/images/arrow.png"></button>
+</template>
+
+<script>
+export default {
+  name: "NextStepButton"
+}
+</script>
+
+<style>
+.step-button {
+  position: absolute;
+  z-index: 2;
+  right: 10%;
+  top: 43%;
+}
+
+.step-button:hover {
+  color: blue;
+}
+</style>
