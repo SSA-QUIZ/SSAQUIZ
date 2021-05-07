1f618b69fd52123a736e1431b3a979fd7dcdc714
chaewon <chechekk0@naver.com>
2021-05-07 17:56:15 +0900

    feat: Add Details for Slide Images
    - 슬라이드에 썸네일을 추가했다.
    - 썸네일을 추가하지 않으면 기본이미지를 띄웠다.

--- frontend/src/components/QuizCreator/QuizSlide.vue
+++ frontend/src/components/QuizCreator/QuizSlide.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="quiz-slide">
     <p class="slide-number">{{ number }}</p>
-    <div class="quiz-img" :class="{ 'quiz-img__selected' : isSelected}">
+    <div class="quiz-img">
+      <img v-if="slide.imagePath" :src="slide.imagePath">
+      <img src="@/assets/images/Default.png">
     </div>
   </div>
 </template>
@@ -9,11 +11,11 @@
 <script>
 export default {
   name: "QuizSlide",
-  props: [
-    'number',
-    'slide',
-    'isSelected'
-  ],
+  props: ['number', 'slide'],
+  data: function () {
+    return {
+    }
+  }
 }
 </script>
 
@@ -21,26 +23,26 @@ export default {
 .quiz-slide {
   display: flex;
   width: 90%;
-	height: 0;
-	overflow: hidden;
-	padding-bottom: 56.26%;
+   height: 0;
+   overflow: hidden;
+   padding-bottom: 56.26%;
 }
 .quiz-slide p {
   margin-left: 10px;
 }
 .quiz-img {
-  background-color: #c4c4c4;
   position: relative;
-	width: 100%;
-	height: 0;
-	overflow: hidden;
-	padding-bottom: 56.26%;
+  width: 100%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 56.26%;
   cursor: pointer;
 }
-.quiz-img__selected {
-	border: 3px solid #E7527E;
-  border-radius: 5px;
+
+.quiz-img img {
+  width: 75%;
 }
+
 .slide-number {
   width: 38px;
   font-family: 'Nanum Pen Script', cursive;
