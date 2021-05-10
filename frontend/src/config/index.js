8663a301caacc5d33f55bf80990be032f21e00e3
chaewon <chechekk0@naver.com>
2021-05-10 16:06:41 +0900

    feat: Add Google login for https
    - https에서도 구글로그인을 할 수 있도록 코드를 변경했다.

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = "https://k4a304.p.ssafy.io/api-auth";
 const ACCESS_TOKEN = "accessToken";
-const OAUTH2_REDIRECT_URI = "http://localhost:8080";
+const OAUTH2_REDIRECT_URI = "https://k4a304.p.ssafy.io/user-page";
 const GOOGLE_AUTH_URL =
   API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
 
