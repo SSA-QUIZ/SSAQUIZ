aa0bc7add51f8fc8d21be62b0ab6f8db5a8fcd28
chaewon <chechekk0@naver.com>
2021-05-13 18:04:52 +0900

    chore: Modify SolvingQuizPage.vue
    - TFChoice 컴포넌트를 수정했다.

--- frontend/src/views/SolvingQuizPage.vue
+++ frontend/src/views/SolvingQuizPage.vue
@@ -5,7 +5,7 @@
     <!-- <OrderingFrame v-show="category == '순서맞히기'" /> -->
     <MultipleChoice @click-button="sendAnswer" :choice="choice" v-if="category == '4지선다' || category == 'vote'" />
     <ShortAnswer v-else-if="category==='단답형'" @click-button="sendAnswer" />
-    <TFChoice v-else-if="category==='TF'" :height="100"/>
+    <TFChoice v-else-if="category==='TF'" :height="75"/>
   </div>
 </template>
 
