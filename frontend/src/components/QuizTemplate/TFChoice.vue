a91ed40eab408afa13e50d736f9192ed2ddad400
장주빈 <sunshine4429@gmail.com>
2021-05-13 19:01:45 +0900

    feat: add functions for T/F in SolvingQuizPage
    - 학생들이 SolvingQuizPage 에서 문제를 풀 때 ws 으로 선택한 답안을 보낼
      수 있도록 함수를 추가했다.

--- frontend/src/components/QuizTemplate/TFChoice.vue
+++ frontend/src/components/QuizTemplate/TFChoice.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="tf-choice">
-    <TrueFalseButton mode="True" width="50%" height=height fontSize="100px" class="tf"/>
-    <TrueFalseButton mode="False" width="50%" height=height fontSize="100px" class="tf"/>
+    <TrueFalseButton @click.native="clickButton(['0', username, quizIndex])" mode="True" width="50%" height=height fontSize="100px" class="tf"/>
+    <TrueFalseButton @click.native="clickButton(['1', username, quizIndex])" mode="False" width="50%" height=height fontSize="100px" class="tf"/>
   </div>
 </template>
 
 <script>
 import TrueFalseButton from '@/components/QuizCreator/TrueFalseButton.vue';
+import { mapState } from 'vuex';
 export default {
   name: "TFChoice",
   props: [
@@ -18,10 +19,19 @@ export default {
   created: function () {
     this.setStyle();
   },
+  computed: {
+    ...mapState("CommonStore", ["isStudent"]),
+    ...mapState("PlayQuizStore", ["username", "quizIndex"]),
+  },
   methods: {
     setStyle: function () {
       if (this.height != undefined) this.style += "height: " + this.height + "%;\n";
-    }
+    },
+    clickButton: function (data) {
+      if (this.isStudent) {
+        this.$emit('click-button', data);
+      }
+    },
   }
 }
 </script>
