16071eb297f118d35c6c0fb37b2432836a1e54f7
최나현 <cnnnnh@gmail.com>
2021-05-16 21:22:51 +0900

    fix: add LoadingPage's mobile version
    - 모바일 환경에서 보이는 로딩페이지의 css 를 추가하였다.

--- frontend/src/views/LoadingPage.vue
+++ frontend/src/views/LoadingPage.vue
@@ -64,31 +64,24 @@ export default {
 @keyframes jello-horizontal {
   0% {
     -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1);
   }
   30% {
     -webkit-transform: scale3d(1.25, 0.75, 1);
-            transform: scale3d(1.25, 0.75, 1);
   }
   40% {
     -webkit-transform: scale3d(0.75, 1.25, 1);
-            transform: scale3d(0.75, 1.25, 1);
   }
   50% {
     -webkit-transform: scale3d(1.15, 0.85, 1);
-            transform: scale3d(1.15, 0.85, 1);
   }
   65% {
     -webkit-transform: scale3d(0.95, 1.05, 1);
-            transform: scale3d(0.95, 1.05, 1);
   }
   75% {
     -webkit-transform: scale3d(1.05, 0.95, 1);
-            transform: scale3d(1.05, 0.95, 1);
   }
   100% {
     -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1);
   }
 }
 .loding-page__animation {
@@ -97,6 +90,12 @@ export default {
   font-family : Jua;
   font-weight: bold;
   color: white;
+}
+
+@media (max-width: 700px) {
+  .loding-page__animation {
+    font-size: 6rem;
+  }
 }
 
 </style>
