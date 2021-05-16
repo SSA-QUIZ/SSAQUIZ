1cd7b0f6687ad57a50ac16f83f89b8975f70f418
최나현 <cnnnnh@gmail.com>
2021-05-16 23:00:32 +0900

    fix: modify NextStepButton
    - 중간점검에 쓰이는 NextStepButton의 스타일을 변경하였다.

--- frontend/src/components/common/NextStepButton.vue
+++ frontend/src/components/common/NextStepButton.vue
@@ -12,14 +12,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .step-button {
-  height: 100px;
+  position: absolute;
+  top: 50%;
+  right: 5%;
 }
 
 .step-button:hover {
   transition: all 0.25s linear;
   transform: scale(1.2);
+}
+
+img {
+  width: 100px;
+  height: 100px;
+}
+
+@media(max-width: 1200px) {
+  img {
+    width: 70px;
+    height: 70px;
+  }
 }
 
 </style>
