7d6b60b0922c99d1fd6a78bea5e717ea47189bf3
장주빈 <sunshine4429@gmail.com>
2021-05-17 15:39:24 +0900

    feat: add password & email validation in Login & Signup Form
    - 로그인 할 때, 회원가입 할 때 이메일 및 패스워드가 적합한지 판단하는
      코드와 그 때 보여주는 alert 창을 추가했다.

--- frontend/src/components/Form/SignupForm.vue
+++ frontend/src/components/Form/SignupForm.vue
@@ -4,7 +4,7 @@
     <InputBox placeholder="닉네임" type="text" @change-input="setNickname" />
     <InputBox placeholder="비밀번호" type="password" @change-input="setPW" />
     <InputBox placeholder="비밀번호 확인" type="password" @change-input="setPWConfirm" />
-    <InputButton @click.native="signup" text="회원가입" />
+    <InputButton @click.native="checkEmail" text="회원가입" />
     <br />
     <a class="hyper-link" @click="moveToLogin">로그인 화면으로 돌아가기</a>
   </div>
@@ -45,8 +45,36 @@ export default {
     setPWConfirm: function (data) {
       this.passwordConfirm = data;
     },
+    checkEmail: function () {
+      var emailRegExp = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
+      if (!emailRegExp.test(this.email)) {
+        this.$emit("signup-result", "email-fail");
+      } else {
+        this.checkPassword();
+      }
+    },
+    checkPassword: function () {
+        var num = this.password.search(/[0-9]/g);
+        var eng = this.password.search(/[a-z]/ig);
+        var spe = this.password.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+        if (this.password.length < 8 || this.password.length > 20) {
+          this.$emit("signup-result", "password-fail-1");
+        }
+        if (this.password.search(/₩s/) != -1) {
+          this.$emit("signup-result", "password-fail-2");
+        }
+        if (num < 0 || eng < 0 || spe < 0 ) {
+          this.$emit("signup-result", "password-fail-3");
+        }
+        if (this.password !== this.passwordConfirm) {
+          this.$emit("signup-result", "password-fail-4");
+        }
+        this.signup();
+    },
     // 회원가입 요청
     signup: function () {
+      this.checkPassword(); 
       if (this.password === this.passwordConfirm) {     // 비밀번호 일치할 경우
         const data = { "email": this.email, "name": this.nickname, "password": this.password }
         axios.post("https://k4a304.p.ssafy.io/api-auth/auth/signup", data)
