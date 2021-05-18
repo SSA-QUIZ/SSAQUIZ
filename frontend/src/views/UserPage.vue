0462ec861428359b03b6a77e3261f25a3c4a563d
장주빈 <sunshine4429@gmail.com>
2021-05-18 10:02:13 +0900

    fix: edit userpage button layout
    - 수정 후 닉네임이 일그러지는 문제가 발생해서 유저페이지 버튼 수정
      사항을 되돌렸다.

--- frontend/src/views/UserPage.vue
+++ frontend/src/views/UserPage.vue
@@ -9,16 +9,14 @@
           <img class="image" :src="profileImg">
         </div>
         <div id="profile__info">
-          <div id="profile__info__container" style="font-size: 5vh; font-weight: 900; font-family: Jua;">
+          <div style="font-size: 5vh; font-weight: 900; font-family: Jua;">
             {{ nickname }}
-            <div id="profile__info__buttons">
-              <button class="pro-button" style="margin-right: 5px; margin-left: 5px;" v-if="googleLogin==='false'" @click="moveToUserInfo">
-                <i class="material-icons">settings</i>
-              </button>
-              <button class="pro-button" @click="logoutConfirm">
-                <i class="material-icons">power_settings_new</i>
-              </button>
-            </div>
+            <button v-if="googleLogin==='false'" style="font-size: 3vh; color: gray;" @click="moveToUserInfo">
+              <i class="material-icons">settings</i>
+            </button>
+            <button style="font-size: 3.5vh; color: gray; margin-left: 10px; margin-top: 3px;" @click="logoutConfirm">
+              <i class="material-icons">power_settings_new</i>
+            </button>
           </div>
           <div><u>{{ email }}</u></div>
         </div>
@@ -300,29 +298,10 @@ export default {
   transform: scale(1.2);
 }
 
-#profile__info__container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
 .quiz-set {
   width: 40%;
   height: 38%;
   margin: 5%;
-}
-
-.pro-button {
-  display: flex;
-  margin-bottom: 5px;
-}
-
-#profile__info__buttons {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  width: 90%;
-  font-size: 3vh;
 }
 
 ::-webkit-scrollbar { width: 30px; }
